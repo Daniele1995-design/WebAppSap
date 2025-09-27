b6e2e4b7f219b91ce552ab6d8e12cd88b7b9877d
Daniele1995-design <daniele.izzo.ats@gmail.com>
2025-09-27 16:30:42 +0200

    Add files via upload

---
+++ Master Installer - WebApp SAP-1.0.0.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Master Installer - WebApp SAP
+// @namespace    https://github.com/tuo-username/tampermonkey-scripts
+// @version      1.0.0
+// @match        http://172.18.20.20/GRN/*
+// @match        http://172.18.20.20:8095/GRN/*
+// @require      https://github.com/Daniele1995-design/WebAppSap/raw/refs/heads/main/Estrazione%20Dati%20MOD%20GRN%20+%20Stampa%20Etichette%2010x10%20e%2010x5.user.js
+// @require      https://www.tampermonkey.net/script_installation.php#url=https://github.com/Daniele1995-design/WebAppSap/raw/refs/heads/main/Ricerca%20PN%20+%20Seriali%20in%20GRN%20-%20WebApp%20SAP.user.js
+// @require      https://www.tampermonkey.net/script_installation.php#url=https://github.com/Daniele1995-design/WebAppSap/raw/refs/heads/main/Ricerca%20Riferimento%20FR%20-%20Seriale%20MOD%20-%20WebApp%20Sap%20(Aggiornato%20con%20Pulizia%20e%20scroll).user.js
+// @require      https://www.tampermonkey.net/script_installation.php#url=https://github.com/Daniele1995-design/WebAppSap/raw/refs/heads/main/Seleziona-Deseleziona%20Tutti%20GRN%20-%20WebApp%20SAP.user.js
+// @require      https://www.tampermonkey.net/script_installation.php#url=https://github.com/Daniele1995-design/WebAppSap/raw/refs/heads/main/Trasferimento%20WH%20Template%20-%20WebApp%20Sap.user.js
+// @grant        none
+// ==/UserScript==
